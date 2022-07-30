70ea0d1332ca05bd86a78f5d7c940e478471c660
Carlos Salame <carlosa@carloss-mbp.lan>
2022-07-30 12:20:48 -0500

    Added basic css styling for component ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,16 @@
+import "./ExpenseItem.css"
+
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
-    
+  return (
+    <div className="expense-item">
+      <div>June 13 2022</div>
+      <div className="expense-item__description">
+        <h2>Mac Miller Vynil</h2>
+      <div className="expense-item__price">$30.00</div>
+    </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
